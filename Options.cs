539941da87e35e53bf2161d411165ef117eb2447
Chrysanthemum Lovelace <chrysanthemum@pitt.edu>
2023-05-25 11:19:50 -0400

    update readme and csproj

--- Options.cs
+++ Options.cs
@@ -1,5 +1,6 @@
 using System;
 using CommandLine;
+using CommandLine.Text;
 
 class Options 
 {
@@ -7,8 +8,14 @@ class Options
     public DateTime? FromDate { get; set; }
     [Option('t', "to", Required = false, HelpText = "To date, defaults to today")]
     public DateTime? ToDate { get; set; }
-    [Value(0)]
+    [Value(0, MetaName = "dataset", Required = true, HelpText = "Which dataset to update (not case sensitive)")]
     public DatasetId DatasetId { get; set; }
+
+    [Usage]
+    public static IEnumerable<Example> Examples {get;} = new List<Example> {
+        new Example("Update Instruction & Outreach dataset", new Options{ DatasetId = DatasetId.InstructionOutreach }),
+        new Example("Update Hillman Head Counts dataset", new Options { DatasetId = DatasetId.HillHeadCounts })
+    };
 }
 
 enum DatasetId
